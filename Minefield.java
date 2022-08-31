fcb29a4a229a54d1a4e6824f9a997e0822d17fdd
Johannes Brodwall <johannes@brodwall.com>
2022-08-31 21:13:15 +0200

    🔵 Minefield should have correct number of rows refactoring

--- Minefield.java
+++ Minefield.java
@@ -2,15 +2,14 @@ public class Minefield {
     private final String[] input;
 
     public Minefield(String[] input) {
-
         this.input = input;
     }
 
     public String[] getHints() {
-        String[] result = new String[input.length];
-        for (int i = 0; i < input.length; i++) {
-            result[i] = "0";
+        String[] hints = new String[input.length];
+        for (int row = 0; row < input.length; row++) {
+            hints[row] = "0";
         }
-        return result;
+        return hints;
     }
 }
