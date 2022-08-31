40170484d6d8e6ca5bf3e30ba3c29b737ad207d8
Johannes Brodwall <johannes@brodwall.com>
2022-08-31 21:12:48 +0200

    🟢 Minefield should have correct number of rows

--- Minefield.java
+++ Minefield.java
@@ -1,9 +1,16 @@
 public class Minefield {
+    private final String[] input;
+
     public Minefield(String[] input) {
 
+        this.input = input;
     }
 
     public String[] getHints() {
-        return new String[] { "0" };
+        String[] result = new String[input.length];
+        for (int i = 0; i < input.length; i++) {
+            result[i] = "0";
+        }
+        return result;
     }
 }
