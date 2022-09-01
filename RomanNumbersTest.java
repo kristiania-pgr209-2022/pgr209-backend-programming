d2ee3efe6ad02416628774d2b9f65c92ba3a6c15
Johannes Brodwall <johannes@brodwall.com>
2022-09-01 13:46:02 +0200

    🔴 Should return I for 1

---
+++ RomanNumbersTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanNumbersTest {
+    @Test
+    void shouldReturnIfor1() {
+        assertEquals("I", toRomanNumber(1));
+    }
+
+    private String toRomanNumber(int i) {
+        return null;
+    }
+}
