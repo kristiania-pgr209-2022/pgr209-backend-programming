640a5f6f29379d01e3077bf13069d1b125c14038
Johannes Brodwall <johannes@brodwall.com>
2022-09-11 21:37:13 +0200

    Make a simple Http request

--- src/main/java/no/kristiania/pgr209/http/HttpClient.java
+++ src/main/java/no/kristiania/pgr209/http/HttpClient.java
@@ -1,8 +1,19 @@
 package no.kristiania.pgr209.http;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class HttpClient {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+        var socket = new Socket("httpbin.org", 80);
+        String request = "GET /html HTTP/1.1\r\n" +
+                   "Host: httpbin.org\r\n" +
+                   "\r\n";
+        socket.getOutputStream().write(request.getBytes());
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            System.out.print((char)c);
+        }
     }
 }
