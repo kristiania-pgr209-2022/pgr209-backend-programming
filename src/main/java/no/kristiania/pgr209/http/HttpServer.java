ee53d3e27c5f8415e783ad65cc13661c47e902c2
Johannes Brodwall <johannes@brodwall.com>
2022-09-11 22:49:26 +0200

    🟢 http server should serve page from disk

--- src/main/java/no/kristiania/pgr209/http/HttpServer.java
+++ src/main/java/no/kristiania/pgr209/http/HttpServer.java
@@ -2,11 +2,12 @@ package no.kristiania.pgr209.http;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class HttpServer {
     private final ServerSocket serverSocket;
-    private Path root;
+    private Path root = Path.of("");
 
     public HttpServer(int serverPort) throws IOException {
         this.serverSocket = new ServerSocket(serverPort);
@@ -25,11 +26,19 @@ public class HttpServer {
             var clientSocket = serverSocket.accept();
             var request = new HttpMessage(clientSocket.getInputStream());
             String requestTarget = request.getStartLine().split(" ")[1];
-            String body = "File not found " + requestTarget;
-            clientSocket.getOutputStream().write(("HTTP/1.1 404 NOT FOUND\r\n" +
-                                                  "Content-Length: " + body.length() + "\r\n" +
-                                                  "\r\n" +
-                                                  body).getBytes());
+            if (Files.exists(root.resolve(requestTarget.substring(1)))) {
+                String body = Files.readString(root.resolve(requestTarget.substring(1)));
+                clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\n" +
+                                                      "Content-Length: " + body.length() + "\r\n" +
+                                                      "\r\n" +
+                                                      body).getBytes());
+            } else {
+                String body = "File not found " + requestTarget;
+                clientSocket.getOutputStream().write(("HTTP/1.1 404 NOT FOUND\r\n" +
+                                                      "Content-Length: " + body.length() + "\r\n" +
+                                                      "\r\n" +
+                                                      body).getBytes());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
