e6e3ae37a227cba5690fba4922dca3049ef28cd3
Johannes Brodwall <johannes@brodwall.com>
2022-09-13 10:00:24 +0200

    Simple HTTP client

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -1,8 +1,24 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class HttpClient {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+        var socket = new Socket("httpbin.org", 80);
+
+        socket.getOutputStream().write(
+                ("GET /html HTTP/1.1\r\n" +
+                 "Connection: close\r\n" +
+                 "Host: httpbin.org\r\n" +
+                 "\r\n").getBytes()
+        );
+
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            System.out.print((char)c);
+        }
+
     }
 }
