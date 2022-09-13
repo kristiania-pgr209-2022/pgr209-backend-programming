9f24536ef6a7443e2bafb4403b09576a2954808c
Johannes Brodwall <johannes@brodwall.com>
2022-09-13 15:42:32 +0200

    Moved Http Message parsing from http request result to http message

--- src/main/java/no/kristiania/http/HttpMessage.java
+++ src/main/java/no/kristiania/http/HttpMessage.java
@@ -1,4 +1,58 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class HttpMessage {
+    private final String startLine;
+    private final Map<String, String> headers;
+    String body;
+    int contentLength;
+
+    public HttpMessage(Socket socket) throws IOException {
+        startLine = readLine(socket);
+        headers = readHeaders(socket);
+        contentLength = Integer.parseInt(getHeader("Content-Length"));
+        body = readBody(socket);
+    }
+
+    public String getStartLine() {
+        return startLine;
+    }
+
+    public String getHeader(String fieldName) {
+        return headers.get(fieldName);
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder line = new StringBuilder();
+        int c;
+        while ((c = socket.getInputStream().read()) != '\r') {
+            line.append((char)c);
+        }
+        c = socket.getInputStream().read(); // read the next \n
+        return line.toString();
+    }
+
+    private Map<String, String> readHeaders(Socket socket) throws IOException {
+        Map<String, String> headers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        String headerLine;
+        while(!(headerLine = readLine(socket)).isEmpty()) {
+            String[] parts = headerLine.split(":\\s*");
+            headers.put(parts[0], parts[1]);
+        }
+        return headers;
+    }
+
+    private String readBody(Socket socket) throws IOException {
+        var body = new byte[contentLength];
+        for (int i = 0; i < body.length; i++) {
+            body[i] = (byte) socket.getInputStream().read();
+        }
+        return new String(body, StandardCharsets.UTF_8);
+    }
+
 }
--- src/main/java/no/kristiania/http/HttpRequestResult.java
+++ src/main/java/no/kristiania/http/HttpRequestResult.java
@@ -3,16 +3,11 @@ package no.kristiania.http;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class HttpRequestResult {
 
-    private HttpMessage response = new HttpMessage();
-    private final Map<String, String> headers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    private final HttpMessage response;
     private final int statusCode;
-    private final int contentLength;
-    private final String body;
 
     public HttpRequestResult(String host, int port, String requestTarget) throws IOException {
         var socket = new Socket(host, port);
@@ -24,32 +19,8 @@ public class HttpRequestResult {
                  "\r\n").getBytes(StandardCharsets.UTF_8)
         );
 
-        String statusLine = readLine(socket);
-        statusCode = Integer.parseInt(statusLine.split(" ")[1]);
-
-        String headerLine;
-        while(!(headerLine = readLine(socket)).isEmpty()) {
-            String[] parts = headerLine.split(":\\s*");
-            headers.put(parts[0], parts[1]);
-        }
-
-        contentLength = Integer.parseInt(getHeader("Content-Length"));
-
-        var body = new byte[contentLength];
-        for (int i = 0; i < body.length; i++) {
-            body[i] = (byte) socket.getInputStream().read();
-        }
-        this.body = new String(body, StandardCharsets.UTF_8);
-    }
-
-    private String readLine(Socket socket) throws IOException {
-        StringBuilder line = new StringBuilder();
-        int c;
-        while ((c = socket.getInputStream().read()) != '\r') {
-            line.append((char)c);
-        }
-        c = socket.getInputStream().read(); // read the next \n
-        return line.toString();
+        response = new HttpMessage(socket);
+        statusCode = Integer.parseInt(response.getStartLine().split(" ")[1]);
     }
 
     public int getStatusCode() {
@@ -57,20 +28,19 @@ public class HttpRequestResult {
     }
 
     public String getHeader(String fieldName) {
-        return headers.get(fieldName);
+        return response.getHeader(fieldName);
     }
 
     public int getContentLength() {
-        return contentLength;
+        return response.contentLength;
     }
 
     public String getBody() {
-        return body;
+        return response.body;
     }
 
     public static void main(String[] args) throws IOException {
         var client = new HttpRequestResult("httpbin.org", 80, "/html");
         System.out.println(client.getBody());
     }
-
 }
