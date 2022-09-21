b1e2a1b1d171ab2ba4aa18e734dab2c88f651158
Johannes Brodwall <johannes@brodwall.com>
2022-09-21 21:55:35 +0200

    🔵 should serve index file

--- src/test/java/no/kristiania/pgr209/http/HttpServerTest.java
+++ src/test/java/no/kristiania/pgr209/http/HttpServerTest.java
@@ -6,16 +6,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpServerTest {
 
     private final HttpServer httpServer = new HttpServer(0);
+    private final Path root = Path.of("target/test-classes/" + UUID.randomUUID());
 
     public HttpServerTest() throws IOException {
         httpServer.start();
-        httpServer.setRoot(Path.of("target/test-classes"));
+        Files.createDirectories(root);
+        httpServer.setRoot(root);
     }
 
     @Test
@@ -28,7 +31,7 @@ public class HttpServerTest {
     @Test
     void shouldServePageFromDisk() throws IOException {
         String fileContent = "A file create at " + LocalDateTime.now();
-        Files.writeString(Path.of("target/test-classes/example-file.txt"), fileContent);
+        Files.writeString(root.resolve("example-file.txt"), fileContent);
 
         var client = new HttpClient("localhost", httpServer.getPort(), "/example-file.txt");
         assertEquals(200, client.getStatus());
@@ -38,7 +41,7 @@ public class HttpServerTest {
     @Test
     void shouldServeMultiplePages() throws IOException {
         String fileContent = "A file create at " + LocalDateTime.now();
-        Files.writeString(Path.of("target/test-classes/other-file.txt"), fileContent);
+        Files.writeString(root.resolve("other-file.txt"), fileContent);
 
         assertEquals(200,
                 new HttpClient("localhost", httpServer.getPort(), "/other-file.txt").getStatus()
@@ -51,7 +54,7 @@ public class HttpServerTest {
     @Test
     void shouldServeIndexFile() throws IOException {
         String fileContent = "A file create at " + LocalDateTime.now();
-        Files.writeString(Path.of("target/test-classes/index.html"), fileContent);
+        Files.writeString(root.resolve("index.html"), fileContent);
 
         var client = new HttpClient("localhost", httpServer.getPort(), "/");
         assertEquals(fileContent, client.getBody());
