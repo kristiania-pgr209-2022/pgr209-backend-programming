808dde128f9c13c0b57b2b8bb6c4db5dcaba551c
Johannes Brodwall <johannes@brodwall.com>
2022-09-21 21:49:41 +0200

    🟢 should serve multiple files

--- src/main/java/no/kristiania/pgr209/http/HttpServer.java
+++ src/main/java/no/kristiania/pgr209/http/HttpServer.java
@@ -2,6 +2,7 @@ package no.kristiania.pgr209.http;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -22,25 +23,31 @@ public class HttpServer {
     }
 
     private void handleClients() {
-        try {
-            var clientSocket = serverSocket.accept();
-            var request = new HttpMessage(clientSocket.getInputStream());
-            String requestTarget = request.getStartLine().split(" ")[1];
-            if (Files.exists(root.resolve(requestTarget.substring(1)))) {
-                String body = Files.readString(root.resolve(requestTarget.substring(1)));
-                clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\n" +
-                                                      "Content-Length: " + body.length() + "\r\n" +
-                                                      "\r\n" +
-                                                      body).getBytes());
-            } else {
-                String body = "File not found " + requestTarget;
-                clientSocket.getOutputStream().write(("HTTP/1.1 404 NOT FOUND\r\n" +
-                                                      "Content-Length: " + body.length() + "\r\n" +
-                                                      "\r\n" +
-                                                      body).getBytes());
+        while (!Thread.interrupted()) {
+            try {
+                var clientSocket = serverSocket.accept();
+                handleRequest(clientSocket);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+    }
+
+    private void handleRequest(Socket clientSocket) throws IOException {
+        var request = new HttpMessage(clientSocket.getInputStream());
+        String requestTarget = request.getStartLine().split(" ")[1];
+        if (Files.exists(root.resolve(requestTarget.substring(1)))) {
+            String body = Files.readString(root.resolve(requestTarget.substring(1)));
+            clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\n" +
+                                                  "Content-Length: " + body.length() + "\r\n" +
+                                                  "\r\n" +
+                                                  body).getBytes());
+        } else {
+            String body = "File not found " + requestTarget;
+            clientSocket.getOutputStream().write(("HTTP/1.1 404 NOT FOUND\r\n" +
+                                                  "Content-Length: " + body.length() + "\r\n" +
+                                                  "\r\n" +
+                                                  body).getBytes());
         }
     }
 
