7f8ed4b7271bee9eb82b7a565762124f42b7da55
Johannes Brodwall <johannes@brodwall.com>
2022-09-22 09:47:52 +0200

    Fix HTTP Server main method

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -43,22 +43,7 @@ public class HttpServer {
     }
 
     public static void main(String[] args) throws IOException {
-        var serverSocket = new ServerSocket(9080);
-
-        var clientSocket = serverSocket.accept();
-
-        var request = new HttpMessage(clientSocket);
-        System.out.println(request.getStartLine());
-        System.out.println(request.headers);
-
-        var body = "<html><h1>Hello world!</h1></html>";
-        var contentLength = body.getBytes().length;
-        clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\n" +
-                                              "Content-Type: text/html\r\n" +
-                                              "Content-Length: " + contentLength + "\r\n" +
-                                              "\r\n" +
-                                              body).getBytes(StandardCharsets.UTF_8));
-
+        var server = new HttpServer(9080);
     }
 
 }
