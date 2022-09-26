df64a0089ee501e6a3de2885a5a25cdb9657bf98
Johannes Brodwall <johannes@brodwall.com>
2022-09-27 00:25:37 +0200

    Dynamic web content support

--- src/main/java/no/kristiania/pgr209/LibraryServer.java
+++ src/main/java/no/kristiania/pgr209/LibraryServer.java
@@ -3,9 +3,12 @@ package no.kristiania.pgr209;
 import no.kristiania.pgr209.books.BookRepository;
 import org.eclipse.jetty.apache.jsp.JettyJasperInitializer;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
+
+import java.io.File;
 
 public class LibraryServer {
 
@@ -18,7 +21,15 @@ public class LibraryServer {
     }
 
     private void start() throws Exception {
-        var webapp = new WebAppContext(Resource.newClassPathResource("/webapp"), "/");
+        var resources = Resource.newClassPathResource("/webapp");
+        var webapp = new WebAppContext(resources, "/");
+
+        var srcDirectory = new File(resources.getFile().toString().replaceAll("\\\\", "/").replace("/target/classes/", "/src/main/resources/"));
+        if (srcDirectory.isDirectory()) {
+            webapp.setBaseResource(Resource.newResource(srcDirectory));
+            webapp.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
+        }
+
         webapp.addServlet(new ServletHolder(new AddBookServlet(bookRepository)), "/api/addBook");
         webapp.addServlet(new ServletHolder(new ListBooksServlet(bookRepository)), "/api/listBooks");
         webapp.addServletContainerInitializer(new JettyJasperInitializer());
