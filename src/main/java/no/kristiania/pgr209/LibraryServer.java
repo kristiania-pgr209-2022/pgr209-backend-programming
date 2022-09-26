3daf34b46d4d4c61d727695d37a59530449658ea
Johannes Brodwall <johannes@brodwall.com>
2022-09-26 23:28:12 +0200

    Add Jetty Server

--- src/main/java/no/kristiania/pgr209/LibraryServer.java
+++ src/main/java/no/kristiania/pgr209/LibraryServer.java
@@ -1,9 +1,17 @@
 package no.kristiania.pgr209;
 
+import org.eclipse.jetty.server.Server;
+
 public class LibraryServer {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    private final Server server = new Server(9080);
+
+    public static void main(String[] args) throws Exception {
+        new LibraryServer().start();
+    }
+
+    private void start() throws Exception {
+        server.start();
     }
 
 }
