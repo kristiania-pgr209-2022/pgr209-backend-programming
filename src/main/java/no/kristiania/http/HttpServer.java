9e5bac662a49b5c28b2746d0f6f6f035a23ac41e
Johannes Brodwall <johannes@brodwall.com>
2022-09-27 09:41:12 +0200

    Very simple HTTP response

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -3,6 +3,7 @@ package no.kristiania.http;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class HttpServer {
 
@@ -28,10 +29,11 @@ public class HttpServer {
     }
 
     private void handleRequest(Socket clientSocket) throws IOException {
-        var response = "Hello world";
+        var response = "<h1>Hållæ verden!</h1>";
         clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\n" +
-                                              "Content-Length: " + response.length() + "\r\n" +
+                                              "Content-Length: " + (response.getBytes(StandardCharsets.UTF_8).length) + "\r\n" +
                                               "Connection: close\r\n" +
-                                              "\r\n" + response).getBytes());
+                                              "Content-Type: text/html; charset=utf-8\r\n" +
+                                              "\r\n" + response).getBytes(StandardCharsets.UTF_8));
     }
 }
