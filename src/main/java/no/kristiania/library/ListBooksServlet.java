452cb0b025daf9e4c078f069172305be1e7d2502
Johannes Brodwall <johannes@brodwall.com>
2022-09-27 15:29:20 +0200

    ListBooksServlet lists all the books in the books repository

--- src/main/java/no/kristiania/library/ListBooksServlet.java
+++ src/main/java/no/kristiania/library/ListBooksServlet.java
@@ -18,11 +18,13 @@ public class ListBooksServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
 
+        resp.getWriter().write("<html>");
         resp.getWriter().write("Here are the books in the category of " + req.getPathInfo());
         resp.getWriter().write("<ul>");
         for (Book book : bookRepository.listAll()) {
             resp.getWriter().write("<li>" + book + "</li>");
         }
         resp.getWriter().write("</ul>");
+        resp.getWriter().write("</html>");
     }
 }
