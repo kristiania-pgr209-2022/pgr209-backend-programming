0e986cfc4b176d64bd9248cb84b6aecf60230629
Johannes Brodwall <johannes@brodwall.com>
2022-09-27 09:53:38 +0200

    Serve a single file

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class HttpServer {
 
@@ -35,12 +37,24 @@ public class HttpServer {
         var requestTarget = parts[1];
 
 
-        var response = "I still haven't found what you're looking for: " + requestTarget;
-        clientSocket.getOutputStream().write(("HTTP/1.1 404 NOT FOUND\r\n" +
-                                              "Content-Length: " + (response.getBytes(StandardCharsets.UTF_8).length) + "\r\n" +
-                                              "Connection: close\r\n" +
-                                              "Content-Type: text/html; charset=utf-8\r\n" +
-                                              "\r\n" + response).getBytes(StandardCharsets.UTF_8));
+        var contentRoot = Path.of("src/main/resources");
+        var requestPath = contentRoot.resolve(requestTarget.substring(1)).toAbsolutePath();
+
+        if (Files.exists(requestPath)) {
+            var response = Files.readString(requestPath);
+            clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\n" +
+                                                  "Content-Length: " + (response.getBytes(StandardCharsets.UTF_8).length) + "\r\n" +
+                                                  "Connection: close\r\n" +
+                                                  "Content-Type: text/html; charset=utf-8\r\n" +
+                                                  "\r\n" + response).getBytes(StandardCharsets.UTF_8));
+        } else {
+            var response = "I still haven't found what you're looking for: " + requestPath;
+            clientSocket.getOutputStream().write(("HTTP/1.1 404 NOT FOUND\r\n" +
+                                                  "Content-Length: " + (response.getBytes(StandardCharsets.UTF_8).length) + "\r\n" +
+                                                  "Connection: close\r\n" +
+                                                  "Content-Type: text/html; charset=utf-8\r\n" +
+                                                  "\r\n" + response).getBytes(StandardCharsets.UTF_8));
+        }
     }
 
     private String readLine(InputStream inputStream) throws IOException {
