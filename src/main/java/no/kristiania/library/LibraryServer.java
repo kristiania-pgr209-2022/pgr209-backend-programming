362e36e8808de0ecfdd115fbef7e08d1f81d25e6
Johannes Brodwall <johannes@brodwall.com>
2022-09-27 14:32:16 +0200

    Running Jetty server

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,7 +1,20 @@
 package no.kristiania.library;
 
+import org.eclipse.jetty.server.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LibraryServer {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    private static final Logger logger = LoggerFactory.getLogger(LibraryServer.class);
+
+    private final Server server = new Server(9080);
+
+    public void start() throws Exception {
+        server.start();
+        logger.info("Started server on {}", server.getURI());
+    }
+
+    public static void main(String[] args) throws Exception {
+        new LibraryServer().start();
     }
 }
