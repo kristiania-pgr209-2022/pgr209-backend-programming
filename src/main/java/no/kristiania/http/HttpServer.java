5e2748723b72333b732ce8b518f6fd2051a7fd6c
Johannes Brodwall <johannes@brodwall.com>
2022-09-27 09:49:14 +0200

    Parse requestLine

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -1,6 +1,7 @@
 package no.kristiania.http;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -29,11 +30,25 @@ public class HttpServer {
     }
 
     private void handleRequest(Socket clientSocket) throws IOException {
-        var response = "<h1>Hållæ verden!</h1>";
-        clientSocket.getOutputStream().write(("HTTP/1.1 200 OK\r\n" +
+        var requestLine = readLine(clientSocket.getInputStream());
+        var parts = requestLine.split(" ");
+        var requestTarget = parts[1];
+
+
+        var response = "I still haven't found what you're looking for: " + requestTarget;
+        clientSocket.getOutputStream().write(("HTTP/1.1 404 NOT FOUND\r\n" +
                                               "Content-Length: " + (response.getBytes(StandardCharsets.UTF_8).length) + "\r\n" +
                                               "Connection: close\r\n" +
                                               "Content-Type: text/html; charset=utf-8\r\n" +
                                               "\r\n" + response).getBytes(StandardCharsets.UTF_8));
+    }
+
+    private String readLine(InputStream inputStream) throws IOException {
+        var line = new StringBuilder();
+        int c;
+        while ((c = inputStream.read()) != '\r') {
+            line.append((char)c);
+        }
+        return line.toString();
     }
 }
