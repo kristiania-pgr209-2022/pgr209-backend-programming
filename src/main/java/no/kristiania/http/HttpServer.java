30aa5ae4989d1abba810f23b953f4f6400b0c187
Johannes Brodwall <johannes@brodwall.com>
2022-09-27 09:26:55 +0200

    Listen to ServerSockegt

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -1,8 +1,27 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class HttpServer {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    private final ServerSocket serverSocket;
+
+    public HttpServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public static void main(String[] args) throws IOException {
+        new HttpServer(9080).start();
+    }
+
+    private void start() {
+        new Thread(() -> {
+            try {
+                serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
