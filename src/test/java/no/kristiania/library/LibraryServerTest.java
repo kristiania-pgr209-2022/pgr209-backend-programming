535875aba4043055e02ed878028ca1ffef8daadb
Johannes Brodwall <johannes@brodwall.com>
2022-10-03 23:23:37 +0200

    Wishful test

---
+++ src/test/java/no/kristiania/library/LibraryServerTest.java
@@ -0,0 +1,21 @@
+package no.kristiania.library;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class LibraryServerTest {
+
+    @Test
+    void shouldShowFrontPage() {
+        LibraryServer server = new LibraryServer(0);
+
+        URL url = server.getURL();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        assertThat(connection.getResponseCode()).as(connection.getResponseMessage())
+                .isEqualTo(200);
+        assertThat(connection.getInputStream()).asString()
+                .contains("<h1>Kristiania Library</h1>");
+    }
+}
