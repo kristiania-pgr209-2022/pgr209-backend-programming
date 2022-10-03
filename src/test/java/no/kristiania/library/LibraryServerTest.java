40bad8634d911237fbe6009d046c3261dc8964ee
Johannes Brodwall <johannes@brodwall.com>
2022-10-03 23:46:39 +0200

    🔴 Library server should return book list

--- src/test/java/no/kristiania/library/LibraryServerTest.java
+++ src/test/java/no/kristiania/library/LibraryServerTest.java
@@ -1,7 +1,9 @@
 package no.kristiania.library;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -10,16 +12,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class LibraryServerTest {
 
-    @Test
-    void shouldShowFrontPage() throws Exception {
-        LibraryServer server = new LibraryServer(0);
-        server.start();
+    private LibraryServer server;
 
-        URL url = server.getURL();
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    @BeforeEach
+    void setUp() throws Exception {
+        server = new LibraryServer(0);
+        server.start();
+    }
+
+    @Test
+    void shouldShowFrontPage() throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL(server.getURL(), "/").openConnection();
         assertThat(connection.getResponseCode()).as(connection.getResponseMessage())
                 .isEqualTo(200);
         assertThat(connection.getInputStream()).asString(StandardCharsets.UTF_8)
                 .contains("<h1>Kristiania Library</h1>");
+    }
+
+    @Test
+    void shouldReturnBookList() throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) new URL(server.getURL(), "/api/books").openConnection();
+        assertThat(connection.getResponseCode()).as(connection.getResponseMessage())
+                .isEqualTo(200);
+        assertThat(connection.getInputStream()).asString(StandardCharsets.UTF_8)
+                .contains("{\"title\": \"Example Book\"");
     }
 }
