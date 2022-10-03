61474ff3ba26096a697c3b6e10dc1859fe7b042d
Johannes Brodwall <johannes@brodwall.com>
2022-10-03 23:49:47 +0200

    🔴 Library server should return book list (return wrong content)
    * Implement ListBooksServlet.doGet

--- src/main/java/no/kristiania/library/ListBooksServlet.java
+++ src/main/java/no/kristiania/library/ListBooksServlet.java
@@ -1,6 +1,14 @@
 package no.kristiania.library;
 
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class ListBooksServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().print("");
+    }
 }
