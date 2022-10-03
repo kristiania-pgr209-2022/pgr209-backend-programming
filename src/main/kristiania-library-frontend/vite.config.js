0409c0a88b65a40466a555997e79cdfc8539024c
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 00:21:04 +0200

    Build React with vite to Jetty resources

--- src/main/kristiania-library-frontend/vite.config.js
+++ src/main/kristiania-library-frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: "/react/",
+  build: {
+    emptyOutDir: true,
+    outDir: "../resources/webapp/react"
+  }
 })
