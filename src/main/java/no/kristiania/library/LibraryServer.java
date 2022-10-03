43627de9d894f95516f11d72613ccbf068ceed7f
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 00:49:26 +0200

    Using src/main/resources instead of target/classes - Fix file locking issue

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,6 +1,7 @@
 package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.slf4j.Logger;
@@ -30,6 +31,7 @@ public class LibraryServer {
                         .replace("/target/classes/", "/src/main/resources/"));
         if (sourcePath.isDirectory()) {
             webapp.setBaseResource(Resource.newResource(sourcePath));
+            webapp.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
         } else {
             webapp.setBaseResource(resource);
         }
