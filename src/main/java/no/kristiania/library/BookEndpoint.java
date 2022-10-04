e9641d2a31a528fd8ca7720ee8ec1a6a5cc82a3d
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 15:40:09 +0200

    🔴 Library server should add book with POST request (parse JSON and add book to books)

--- src/main/java/no/kristiania/library/BookEndpoint.java
+++ src/main/java/no/kristiania/library/BookEndpoint.java
@@ -2,9 +2,12 @@ package no.kristiania.library;
 
 import jakarta.json.Json;
 import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,5 +32,17 @@ public class BookEndpoint {
             );
         }
         return Response.ok(result.build().toString()).build();
+    }
+
+    @Path("/")
+    @POST
+    public Response addBook(String body) {
+        JsonObject jsonBook = Json.createReader(new StringReader(body)).readObject();
+        var book = new Book();
+        book.setAuthor(jsonBook.getString("author"));
+        book.setTitle(jsonBook.getString("title"));
+        books.add(book);
+
+        return Response.ok().build();
     }
 }
