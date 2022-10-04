09243e8d7163edcc2bc528d8e78a5730aa778ecb
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 10:55:50 +0200

    🟢 Library server should list books (uses generated JSON)

--- src/main/java/no/kristiania/library/ListBooksServlet.java
+++ src/main/java/no/kristiania/library/ListBooksServlet.java
@@ -18,19 +18,12 @@ public class ListBooksServlet extends HttpServlet {
         var books = List.of(exampleBook);
 
         JsonArrayBuilder result = Json.createArrayBuilder();
-        resp.getWriter().write("[");
         for (Book book : books) {
             result.add(Json.createObjectBuilder()
                     .add("title", book.getTitle())
                     .add("author", book.getAuthor())
             );
-
-            resp.getWriter().write("{");
-            resp.getWriter().write("\"title\":\"" + book.getTitle() + "\"");
-            resp.getWriter().write("\"author\":\"" + book.getAuthor() + "\"");
-            resp.getWriter().write("}");
         }
-        resp.getWriter().write("]");
-        result.build().toString();
+        resp.getWriter().write(result.build().toString());
     }
 }
