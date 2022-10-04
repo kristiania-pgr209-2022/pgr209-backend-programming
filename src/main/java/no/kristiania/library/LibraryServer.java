6860b49affaa82756e7c067553d7424b390bd5ba
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 10:31:41 +0200

    Fix logging

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -3,11 +3,15 @@ package no.kristiania.library;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class LibraryServer {
+    private static final Logger logger = LoggerFactory.getLogger(LibraryServer.class);
+
     private final Server server;
 
     public LibraryServer(int port) {
@@ -26,5 +30,11 @@ public class LibraryServer {
 
     public void start() throws Exception {
         server.start();
+    }
+
+    public static void main(String[] args) throws Exception {
+        var server = new LibraryServer(8080);
+        server.start();
+        logger.warn("Server starting at {}", server.getURL());
     }
 }
