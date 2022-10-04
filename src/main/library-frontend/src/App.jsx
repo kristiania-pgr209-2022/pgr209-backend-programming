0503ecbcb32542cd40d4019d637209de51b96631
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 14:49:46 +0200

    Add BookList component in React

--- src/main/library-frontend/src/App.jsx
+++ src/main/library-frontend/src/App.jsx
@@ -1,24 +1,32 @@
+import "./App.css";
 import { useEffect, useState } from "react";
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+function BookList() {
+  const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("/api/books");
+      setBooks(await res.json());
+      setLoading(false);
+    })();
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  return <ul>{books.map(b => <div>{b.title}</div>)}</ul>;
+}
 
 function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Kristiania Library</h1>
+      <h1>Library</h1>
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BookList />
     </div>
   )
 }
