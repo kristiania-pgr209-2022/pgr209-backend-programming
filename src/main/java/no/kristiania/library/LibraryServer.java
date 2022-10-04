4e93a4576577e7dc7fa3c0ac3e795e553053a768
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 14:44:49 +0200

    Magic incantation to get Jetty not to lock files

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,6 +1,7 @@
 package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
@@ -29,6 +30,7 @@ public class LibraryServer {
                 .replace("target/classes", "src/main/resources"));
         if (sourceDirectory.isDirectory()) {
             webContext.setBaseResource(Resource.newResource(sourceDirectory));
+            webContext.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
         } else {
             webContext.setBaseResource(resources);
         }
