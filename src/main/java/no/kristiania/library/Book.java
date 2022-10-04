5d2500c4b699d605f8c8682b684fa4bcbc3dcbb5
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 10:47:26 +0200

    🟢 Library server should list books
    * Add class Book

---
+++ src/main/java/no/kristiania/library/Book.java
@@ -0,0 +1,22 @@
+package no.kristiania.library;
+
+public class Book {
+    private String title;
+    private String author;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
--- src/main/java/no/kristiania/library/ListBooksServlet.java
+++ src/main/java/no/kristiania/library/ListBooksServlet.java
@@ -5,13 +5,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class ListBooksServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var book = new Book();
+        book.setTitle("Java in a nutshell");
+        book.setAuthor("David Flannagan");
+        var books = List.of(book);
+
 
         resp.getWriter().println("[{\"title\":\"Java in a nutshell\"}]");
-
     }
 }
