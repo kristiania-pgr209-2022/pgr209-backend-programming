4560e83cb673c86e1a337c71cba9f96387fcb470
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 15:54:06 +0200

    Frontend add book

--- src/main/library-frontend/src/App.jsx
+++ src/main/library-frontend/src/App.jsx
@@ -20,6 +20,32 @@ function BookList() {
   return <ul>{books.map(b => <div>{b.title}</div>)}</ul>;
 }
 
+function AddBook() {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await fetch("/api/books", {
+      method: "post",
+      body: JSON.stringify({title, author}),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+
+  return <div>
+    <form onSubmit={handleSubmit}>
+      <div><label>Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} /></label></div>
+      <div><label>Author: <input type="text" value={author} onChange={e => setAuthor(e.target.value)} /></label></div>
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
+  </div>;
+}
+
 function App() {
 
   return (
@@ -27,6 +53,7 @@ function App() {
       <h1>Library</h1>
 
       <BookList />
+      <AddBook />
     </div>
   )
 }
