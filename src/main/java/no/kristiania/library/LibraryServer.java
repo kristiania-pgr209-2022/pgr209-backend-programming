fbe5f8d155b15642d04cf6368eb08a49438e7a90
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 09:58:04 +0200

    🔴 Library server should serve home page (wrong content)
    * server.setHandler with Resource.fromClassPath

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,6 +1,8 @@
 package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,6 +12,12 @@ public class LibraryServer {
 
     public LibraryServer(int port) {
         this.server = new Server(port);
+
+        WebAppContext webContext = new WebAppContext();
+        webContext.setContextPath("/");
+        webContext.setBaseResource(Resource.newClassPathResource("/webapp"));
+        server.setHandler(webContext);
+
     }
 
     public URL getURL() throws MalformedURLException {
