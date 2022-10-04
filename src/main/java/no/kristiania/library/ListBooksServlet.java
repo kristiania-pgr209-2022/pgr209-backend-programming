99ee47390df4e45ce1147c4e046d2d4b72a5e09b
Johannes Brodwall <johannes@brodwall.com>
2022-10-04 10:44:02 +0200

    🟢 Library server should list books
    * Implement ListBooksServlet.doGet

--- src/main/java/no/kristiania/library/ListBooksServlet.java
+++ src/main/java/no/kristiania/library/ListBooksServlet.java
@@ -1,6 +1,17 @@
 package no.kristiania.library;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class ListBooksServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.getWriter().println("[{\"title\":\"Java in a nutshell\"}]");
+
+    }
 }
