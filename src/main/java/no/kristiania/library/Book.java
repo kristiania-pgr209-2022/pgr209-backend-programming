a4b6a62e01e7e09aa7b7f30b04e8cfbaf7ddccc6
Johannes Brodwall <johannes@brodwall.com>
2022-10-09 23:42:11 +0200

    Package as executable jar-file

--- src/main/java/no/kristiania/library/Book.java
+++ src/main/java/no/kristiania/library/Book.java
@@ -1,4 +1,35 @@
 package no.kristiania.library;
 
-public record Book(String title) {
+import java.util.Objects;
+
+public final class Book {
+    private final String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Book) obj;
+        return Objects.equals(this.title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
+    }
+
+    @Override
+    public String toString() {
+        return "Book[" +
+               "title=" + title + ']';
+    }
+
 }
