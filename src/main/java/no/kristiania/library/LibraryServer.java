51e89544439ed1ad99746fd3af0618a0bf015b7a
Johannes Brodwall <johannes@brodwall.com>
2022-10-09 23:09:27 +0200

    🟢 Should serve content from endpoint ("cheating" with servlet)

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,10 +1,18 @@
 package no.kristiania.library;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,8 +24,21 @@ public class LibraryServer {
         this.server = new Server(port);
 
         server.setHandler(new HandlerList(
+                createApiContext(),
                 new WebAppContext(Resource.newClassPathResource("/webapp"), "/")
         ));
+    }
+
+    private Handler createApiContext() {
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/api");
+        context.addServlet(new ServletHolder(new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.getWriter().write("Hello World");
+            }
+        }), "/hello");
+        return context;
     }
 
     void start() throws Exception {
