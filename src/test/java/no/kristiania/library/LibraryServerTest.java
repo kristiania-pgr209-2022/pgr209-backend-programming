b6e53c7f8020a9325a79f659d713d3bb1c82e8d6
Johannes Brodwall <johannes@brodwall.com>
2022-10-11 09:41:15 +0200

    🔵 Library server should show front page

--- src/test/java/no/kristiania/library/LibraryServerTest.java
+++ src/test/java/no/kristiania/library/LibraryServerTest.java
@@ -1,24 +1,34 @@
 package no.kristiania.library;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LibraryServerTest {
 
+    private LibraryServer server;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        server = new LibraryServer(0);
+        server.start();
+    }
+
     @Test
     void shouldShowFrontPage() throws Exception {
-        var server = new LibraryServer(0);
-        server.start();
-        var connection = (HttpURLConnection)server.getURL().openConnection();
+        var connection = openConnection();
         assertThat(connection.getResponseCode()).isEqualTo(200);
         assertThat(connection.getInputStream())
                 .asString(StandardCharsets.UTF_8)
                 .contains("<title>Kristiania Library</title>");
+    }
+
+    private HttpURLConnection openConnection() throws IOException {
+        return (HttpURLConnection) server.getURL().openConnection();
     }
 }
