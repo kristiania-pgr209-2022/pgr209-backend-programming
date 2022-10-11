af93b75c992b8b80fff9a5cf41c7a514fefe0bd6
Johannes Brodwall <johannes@brodwall.com>
2022-10-11 15:31:27 +0200

    🟢 Library server should add book with HTTP POST (BookRepository must be singleton)

--- src/main/java/no/kristiania/library/LibraryConfig.java
+++ src/main/java/no/kristiania/library/LibraryConfig.java
@@ -3,13 +3,16 @@ package no.kristiania.library;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import jakarta.inject.Singleton;
+
 class LibraryConfig extends ResourceConfig {
     public LibraryConfig() {
         super(BookResource.class);
         register(new AbstractBinder() {
             @Override
             protected void configure() {
-                bind(BookRepositoryImpl.class).to(BookRepository.class);
+                bind(BookRepositoryImpl.class).to(BookRepository.class)
+                        .in(Singleton.class);
             }
         });
     }
