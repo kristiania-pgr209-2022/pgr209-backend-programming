faab5cb7d93a74134c90e39929dc4b23e1703651
Johannes Brodwall <johannes@brodwall.com>
2022-10-11 09:31:11 +0200

    🔴 Library server should show front page (test now compiles)

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,8 +1,23 @@
 package no.kristiania.library;
 
+import org.eclipse.jetty.server.Server;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class LibraryServer {
+
+    private final Server server;
+
+    public LibraryServer(int port) {
+        this.server = new Server(0);
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+    }
+
+    public URL getURL() throws MalformedURLException {
+        return server.getURI().toURL();
     }
 }
--- src/test/java/no/kristiania/library/LibraryServerTest.java
+++ src/test/java/no/kristiania/library/LibraryServerTest.java
@@ -2,7 +2,9 @@ package no.kristiania.library;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,12 +12,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LibraryServerTest {
 
     @Test
-    void shouldShowFrontPage() {
+    void shouldShowFrontPage() throws IOException {
         var server = new LibraryServer(0);
         var connection = (HttpURLConnection)server.getURL().openConnection();
         assertThat(connection.getResponseCode()).isEqualTo(200);
         assertThat(connection.getInputStream())
                 .asString(StandardCharsets.UTF_8)
-                .contain("<title>Kristiania Library</title>");
+                .contains("<title>Kristiania Library</title>");
     }
 }
