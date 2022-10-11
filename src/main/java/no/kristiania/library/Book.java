2c3fe83c199486660845287863347304c4f50e98
Johannes Brodwall <johannes@brodwall.com>
2022-10-11 15:28:02 +0200

    🔴 Library server should add book with HTTP POST (default constructor for Book, but posted book not included)

--- src/main/java/no/kristiania/library/Book.java
+++ src/main/java/no/kristiania/library/Book.java
@@ -1,13 +1,13 @@
 package no.kristiania.library;
 
 public class Book {
-    private final String title;
-
-    public Book(String title) {
-        this.title = title;
-    }
+    private String title;
 
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
--- src/main/java/no/kristiania/library/BookRepositoryImpl.java
+++ src/main/java/no/kristiania/library/BookRepositoryImpl.java
@@ -5,8 +5,14 @@ import java.util.List;
 
 public class BookRepositoryImpl implements BookRepository {
     private final List<Book> books = new ArrayList<>(
-            List.of(new Book("Hello World"))
+            List.of(sampleBook())
     );
+
+    private static Book sampleBook() {
+        var book = new Book();
+        book.setTitle("Hello World");
+        return book;
+    }
 
     @Override
     public List<Book> listAll() {
