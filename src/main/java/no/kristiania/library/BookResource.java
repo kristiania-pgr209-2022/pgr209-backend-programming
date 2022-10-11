d2957aedf8484c1a93b5c07dab47f86fc6d95113
Johannes Brodwall <johannes@brodwall.com>
2022-10-11 10:31:04 +0200

    🔴 Library server should add book (book is not saved)

--- src/main/java/no/kristiania/library/BookResource.java
+++ src/main/java/no/kristiania/library/BookResource.java
@@ -1,9 +1,11 @@
 package no.kristiania.library;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/books")
@@ -13,5 +15,10 @@ public class BookResource {
     @Produces(MediaType.APPLICATION_JSON)
     public List<Book> getAllBooks() {
         return List.of(new Book("Hello World"));
+    }
+
+    @POST
+    public Response saveBook() {
+        return Response.ok().build();
     }
 }
