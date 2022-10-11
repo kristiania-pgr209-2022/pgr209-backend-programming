9ca02ffc9a0d829f37041df6165bd6c8a37f548f
Johannes Brodwall <johannes@brodwall.com>
2022-10-11 10:22:29 +0200

    🔴 Library server should server dynamic content (try to return POJO)

---
+++ src/main/java/no/kristiania/library/Book.java
@@ -0,0 +1,13 @@
+package no.kristiania.library;
+
+public class Book {
+    private final String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
--- src/main/java/no/kristiania/library/BookResource.java
+++ src/main/java/no/kristiania/library/BookResource.java
@@ -4,13 +4,14 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/books")
 public class BookResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getAllBooks() {
-        return "{\"title\":\"Hello World\"";
+    public List<Book> getAllBooks() {
+        return List.of(new Book("Hello World"));
     }
 }
