064ec113f3e59f5827f294756a66f0cd4fe9db98
Johannes Brodwall <johannes@brodwall.com>
2022-10-11 10:18:15 +0200

    🟢 Library server should server dynamic content (serve application/json)

--- src/main/java/no/kristiania/library/BookResource.java
+++ src/main/java/no/kristiania/library/BookResource.java
@@ -2,11 +2,14 @@ package no.kristiania.library;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("/books")
 public class BookResource {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public String getAllBooks() {
         return "{\"title\":\"Hello World\"";
     }
