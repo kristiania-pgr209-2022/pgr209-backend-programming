c93e62d2392409252146f8efdacf1042591667df
Johannes Brodwall <johannes@brodwall.com>
2022-10-17 23:21:25 +0200

    🟢 Book dao should retrieve saved book (db_close_delay)

--- src/test/java/no/kristiania/library/database/BookDaoTest.java
+++ src/test/java/no/kristiania/library/database/BookDaoTest.java
@@ -16,7 +16,7 @@ public class BookDaoTest {
     @BeforeEach
     void setUp() throws SQLException {
         var dataSource = new JdbcDataSource();
-        dataSource.setUrl("jdbc:h2:mem:bookDaoTest");
+        dataSource.setUrl("jdbc:h2:mem:bookDaoTest;DB_CLOSE_DELAY=-1");
 
         try (Connection connection = dataSource.getConnection()) {
             var statement = connection.createStatement();
