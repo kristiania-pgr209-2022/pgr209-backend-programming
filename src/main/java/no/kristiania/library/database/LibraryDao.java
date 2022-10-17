4af8f702a13d275e3f0bc33d0a4ac4fbbd6b58fc
Johannes Brodwall <johannes@brodwall.com>
2022-10-17 23:49:24 +0200

    🔴 Library dao should retrieve saved library

---
+++ src/main/java/no/kristiania/library/database/LibraryDao.java
@@ -0,0 +1,16 @@
+package no.kristiania.library.database;
+
+import javax.sql.DataSource;
+
+public class LibraryDao {
+    public LibraryDao(DataSource dataSource) {
+    }
+
+    public Library retrieve(Long id) {
+        return null;
+    }
+
+    public void save(Library library) {
+
+    }
+}
---
+++ src/test/java/no/kristiania/library/database/LibraryDaoTest.java
@@ -0,0 +1,40 @@
+package no.kristiania.library.database;
+
+import org.flywaydb.core.Flyway;
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LibraryDaoTest {
+
+    private LibraryDao dao;
+
+    @BeforeEach
+    void setUp() {
+        var dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:libraryDaoTest;DB_CLOSE_DELAY=-1");
+
+        var flyway = Flyway.configure().dataSource(dataSource).load();
+        flyway.migrate();
+
+        dao = new LibraryDao(dataSource);
+    }
+
+    @Test
+    void shouldRetrieveSavedLibrary() {
+        var library = sampleLibrary();
+        dao.save(library);
+        assertThat(dao.retrieve(library.getId()))
+                .usingRecursiveComparison()
+                .isEqualTo(library);
+    }
+
+    private Library sampleLibrary() {
+        var library = new Library();
+        library.setName("Deichmanske");
+        library.setAddress("Dronning Eufemias gate");
+        return library;
+    }
+}
