9dd594822c89caf806c0633a419bb79b3ff05e03
Johannes Brodwall <johannes@brodwall.com>
2022-10-17 23:29:34 +0200

    🟢 Book dao should retrieve saved book (moved create table to migration file)

--- src/test/java/no/kristiania/library/database/BookDaoTest.java
+++ src/test/java/no/kristiania/library/database/BookDaoTest.java
@@ -5,7 +5,6 @@ import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,17 +14,12 @@ public class BookDaoTest {
     private BookDao dao;
 
     @BeforeEach
-    void setUp() throws SQLException {
+    void setUp() {
         var dataSource = new JdbcDataSource();
         dataSource.setUrl("jdbc:h2:mem:bookDaoTest;DB_CLOSE_DELAY=-1");
 
         var flyway = Flyway.configure().dataSource(dataSource).load();
         flyway.migrate();
-
-        try (Connection connection = dataSource.getConnection()) {
-            var statement = connection.createStatement();
-            statement.executeUpdate("create table books (id serial primary key, title varchar(100), author_name varchar(100))");
-        }
 
         dao = new BookDao(dataSource);
     }
