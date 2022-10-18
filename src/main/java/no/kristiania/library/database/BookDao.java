ec05ebdfc04c74c20125082fe6b3a310e9fa9c5d
Johannes Brodwall <johannes@brodwall.com>
2022-10-18 14:27:18 +0200

    🟢 Book dao should find books by author name
    * Extract method readBook
    * Implement method findAuthorName with executeQuery and while (rs.next())

--- src/main/java/no/kristiania/library/database/BookDao.java
+++ src/main/java/no/kristiania/library/database/BookDao.java
@@ -1,6 +1,7 @@
 package no.kristiania.library.database;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -31,17 +32,12 @@ public class BookDao {
     }
 
     public Book retrieve(long id) throws SQLException {
-
         try (var connection = dataSource.getConnection()) {
             try (var statement = connection.prepareStatement("select * from books where id = ?")) {
                 statement.setLong(1, id);
                 try (var rs = statement.executeQuery()) {
                     if (rs.next()) {
-                        var book = new Book();
-                        book.setId(rs.getLong("id"));
-                        book.setTitle(rs.getString("title"));
-                        book.setAuthorName(rs.getString("author_name"));
-                        return book;
+                        return readBook(rs);
                     } else {
                         return null;
                     }
@@ -50,7 +46,27 @@ public class BookDao {
         }
     }
 
-    public List<Book> findByAuthorName(String authorName) {
-        return new ArrayList<>();
+    public List<Book> findByAuthorName(String authorName) throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            try (var statement = connection.prepareStatement("select * from books where author_name = ?")) {
+                statement.setString(1, authorName);
+                try (var rs = statement.executeQuery()) {
+                    var books = new ArrayList<Book>();
+                    while (rs.next()) {
+                        books.add(readBook(rs));
+                    }
+                    return books;
+                }
+            }
+        }
     }
+
+    private Book readBook(ResultSet rs) throws SQLException {
+        var book = new Book();
+        book.setId(rs.getLong("id"));
+        book.setTitle(rs.getString("title"));
+        book.setAuthorName(rs.getString("author_name"));
+        return book;
+    }
+
 }
