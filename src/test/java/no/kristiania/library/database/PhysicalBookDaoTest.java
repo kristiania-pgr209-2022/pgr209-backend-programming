78baf65078f9435d3648a05ae402e40f2f5598e6
Johannes Brodwall <johannes@brodwall.com>
2022-10-18 15:21:32 +0200

    🔴 Physical book dao should retrieve books from library (insert books and libraries)

--- src/test/java/no/kristiania/library/database/PhysicalBookDaoTest.java
+++ src/test/java/no/kristiania/library/database/PhysicalBookDaoTest.java
@@ -2,19 +2,30 @@ package no.kristiania.library.database;
 
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
+
+import java.sql.SQLException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PhysicalBookDaoTest {
 
-    private final PhysicalBookDao dao = new PhysicalBookDao(InMemoryDataSource.createTestDataSource());
+    private final DataSource dataSource = InMemoryDataSource.createTestDataSource();
+    private final LibraryDao libraryDao = new LibraryDao(dataSource);
+    private final BookDao bookDao = new BookDao(dataSource);
+    private final PhysicalBookDao dao = new PhysicalBookDao(dataSource);
 
     @Test
-    void shouldListBooksByLibrary() {
+    void shouldListBooksByLibrary() throws SQLException {
         var firstBook = SampleData.sampleBook();
         var secondBook = SampleData.sampleBook();
+        bookDao.save(firstBook);
+        bookDao.save(secondBook);
 
         var firstLibrary = SampleData.sampleLibrary();
         var secondLibrary = SampleData.sampleLibrary();
+        libraryDao.save(firstLibrary);
+        libraryDao.save(secondLibrary);
 
         dao.insert(firstLibrary, firstBook);
         dao.insert(firstLibrary, secondBook);
