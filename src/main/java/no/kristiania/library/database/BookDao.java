05176fae5cbc4f5c4dabe32971cd91883e3cc34f
Johannes Brodwall <johannes@brodwall.com>
2022-10-18 09:54:59 +0200

    🟢 Book dao should retrieve saved book (getGeneratedKeys with RETURN_GENERATED_KEYS)

--- src/main/java/no/kristiania/library/database/BookDao.java
+++ src/main/java/no/kristiania/library/database/BookDao.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,7 +21,7 @@ public class BookDao {
     public void save(Book book) throws SQLException {
         try (var connection = dataSource.getConnection()) {
             var sql = "insert into books (title) values (?)";
-            try (var statement = connection.prepareStatement(sql)) {
+            try (var statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
                 statement.setString(1, book.getTitle());
                 statement.executeUpdate();
 
