4d92127701f5379bfbf3c7999851edff60a9261d
Johannes Brodwall <johannes@brodwall.com>
2022-10-18 18:07:48 +0200

    JPA stuff - persistence.xml an @Entity annotations

--- src/main/java/no/kristiania/library/database/Library.java
+++ src/main/java/no/kristiania/library/database/Library.java
@@ -1,9 +1,16 @@
 package no.kristiania.library.database;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Library {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
     private String address;
-    private Long id;
 
     public void setName(String name) {
         this.name = name;
