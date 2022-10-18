78f60576a88d8a5420d9bb4225a837c39cffe9bc
Johannes Brodwall <johannes@brodwall.com>
2022-10-18 18:22:43 +0200

    🔴 Library server should get posted content (doesn't even compile)

---
+++ src/test/java/no/kristiania/library/LibraryServerTest.java
@@ -0,0 +1,35 @@
+package no.kristiania.library;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LibraryServerTest {
+
+    @Test
+    void shouldGetPostedResources() throws IOException {
+        var server = new LibraryServer(0);
+        var url = new URL(server.getURL(), "/api/books");
+
+        var postConnection = (HttpURLConnection)url.openConnection();
+        postConnection.setRequestMethod("POST");
+        postConnection.setDoOutput(true);
+        //language=JSON
+        postConnection.getOutputStream().write("""
+        {"name":"test"}
+        """.getBytes());
+        assertThat(postConnection.getResponseCode()).as(postConnection.getRequestMethod())
+                .isEqualTo(204);
+
+        var getConnection = (HttpURLConnection)url.openConnection();
+        assertThat(getConnection.getResponseCode()).as(getConnection.getRequestMethod())
+                .isEqualTo(200);
+        assertThat(getConnection.getInputStream()).asString(UTF_8).contains("""
+                        {"name":"test"}""");
+    }
+}
