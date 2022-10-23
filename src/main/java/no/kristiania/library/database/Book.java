4e907429e4a85abfd1c645cdfc7fd4693e0e30ba
Johannes Brodwall <johannes@brodwall.com>
2022-10-23 20:58:25 +0200

    🟢🔴 JpaBookDao should retrieve saved book (specify @Table and @Column name for Book)

--- src/main/java/no/kristiania/library/database/Book.java
+++ src/main/java/no/kristiania/library/database/Book.java
@@ -1,16 +1,21 @@
 package no.kristiania.library.database;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(name = "author_name")
     private String authorName;
 
     public Long getId() {
