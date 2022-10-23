4950f835263ce832af5c26e50aa51ea0bc3d2e9d
Johannes Brodwall <johannes@brodwall.com>
2022-10-23 20:36:39 +0200

    🟢 JPA Library DAO should retrieve saved libraries (beginning and rolling back transaction)

--- src/test/java/no/kristiania/library/database/jpa/TestPersistenceManager.java
+++ src/test/java/no/kristiania/library/database/jpa/TestPersistenceManager.java
@@ -2,6 +2,8 @@ package no.kristiania.library.database.jpa;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
@@ -11,10 +13,12 @@ import org.junit.jupiter.api.extension.ParameterResolver;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
+
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(TestPersistenceManager.PersistenceManagerResolver.class)
 public @interface TestPersistenceManager {
-    class PersistenceManagerResolver implements ParameterResolver {
+    class PersistenceManagerResolver implements ParameterResolver, BeforeEachCallback, AfterEachCallback {
 
         @Override
         public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
@@ -23,7 +27,21 @@ public @interface TestPersistenceManager {
 
         @Override
         public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-            return Persistence.createEntityManagerFactory("library").createEntityManager();
+            var entityManager = Persistence.createEntityManagerFactory("library").createEntityManager();
+            extensionContext.getStore(GLOBAL).put(EntityManager.class.getName(), entityManager);
+            return entityManager;
+        }
+
+        @Override
+        public void beforeEach(ExtensionContext context) {
+            context.getStore(GLOBAL).get(EntityManager.class.getName(), EntityManager.class)
+                    .getTransaction().begin();
+        }
+
+        @Override
+        public void afterEach(ExtensionContext context) {
+            context.getStore(GLOBAL).get(EntityManager.class.getName(), EntityManager.class)
+                    .getTransaction().rollback();
         }
     }
 }
