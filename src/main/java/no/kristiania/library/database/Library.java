a00f56866ca4fd715fa5b55feb8c543c56f16ab7
Johannes Brodwall <johannes@brodwall.com>
2022-10-23 20:21:37 +0200

    🟢 JPA Library DAO should retrieve saved libraries
    * But lots of future duplication in the test
    * But hardcoded password in persistence.xml

--- src/main/java/no/kristiania/library/database/Library.java
+++ src/main/java/no/kristiania/library/database/Library.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "libraries")
 public class Library {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
