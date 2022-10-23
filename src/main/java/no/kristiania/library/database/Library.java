c26ee657202dd506f6524ce477996e1d35034ab2
Johannes Brodwall <johannes@brodwall.com>
2022-10-23 20:17:02 +0200

    🔴 JPA Library DAO should retrieve saved libraries (Library.getId is null)

--- src/main/java/no/kristiania/library/database/Library.java
+++ src/main/java/no/kristiania/library/database/Library.java
@@ -1,9 +1,17 @@
 package no.kristiania.library.database;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Library {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String address;
-    private Long id;
 
     public void setName(String name) {
         this.name = name;
