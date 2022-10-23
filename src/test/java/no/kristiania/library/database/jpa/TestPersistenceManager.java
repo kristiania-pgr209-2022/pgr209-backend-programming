ba99d0b77846c6a5960181edbb2f5eb3699fa01b
Johannes Brodwall <johannes@brodwall.com>
2022-10-23 20:44:21 +0200

    🟢 JPA Library DAO should retrieve saved libraries
    * Using data source from Demo

--- src/test/java/no/kristiania/library/database/jpa/TestPersistenceManager.java
+++ src/test/java/no/kristiania/library/database/jpa/TestPersistenceManager.java
@@ -1,7 +1,10 @@
 package no.kristiania.library.database.jpa;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Persistence;
+import org.eclipse.jetty.plus.jndi.Resource;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,8 +13,12 @@ import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 
+import javax.naming.NamingException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
 
@@ -30,11 +37,32 @@ public @interface TestPersistenceManager {
 
         @Override
         public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+            try {
+                new Resource("jdbc/dataSource", createDataSource());
+            } catch (NamingException | IOException e) {
+                throw new ParameterResolutionException(e.toString());
+            }
+
             var persistenceUnitName = parameterContext.getParameter().getAnnotation(TestPersistenceManager.class).value();
             var entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
             var entityManager = entityManagerFactory.createEntityManager();
             extensionContext.getStore(GLOBAL).put(EntityManager.class.getName(), entityManager);
             return entityManager;
+        }
+
+        private Object createDataSource() throws IOException {
+            var properties = new Properties();
+            try (var reader = new FileReader("application.properties")) {
+                properties.load(reader);
+            }
+
+            var dataSource = new HikariDataSource();
+            dataSource.setJdbcUrl(properties.getProperty("jdbc.url", "jdbc:postgresql://localhost:5432/library"));
+            dataSource.setUsername(properties.getProperty("jdbc.username", "kristiania_app"));
+            dataSource.setPassword(properties.getProperty("jdbc.password"));
+            Flyway.configure().dataSource(dataSource).load().migrate();
+
+            return dataSource;
         }
 
         @Override
