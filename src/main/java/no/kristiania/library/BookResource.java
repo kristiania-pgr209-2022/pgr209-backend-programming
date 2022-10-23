5c95272ba6de930e87aabc9f717326bb1fee99d8
Johannes Brodwall <johannes@brodwall.com>
2022-10-23 22:18:40 +0200

    🔴 LibraryServerTest should post new book ("MessageBodyWriter not found for media type=text/plain")

--- src/main/java/no/kristiania/library/BookResource.java
+++ src/main/java/no/kristiania/library/BookResource.java
@@ -1,10 +1,23 @@
 package no.kristiania.library;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import no.kristiania.library.database.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("/books")
 public class BookResource {
+
+    private static final ArrayList<Book> books = new ArrayList<>();
+
+    @GET
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
     @POST
     public void addBook() {
 
