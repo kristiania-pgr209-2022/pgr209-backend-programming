f252dd737cae2819874f50c458a013f1aeeff3bb
Johannes Brodwall <johannes@brodwall.com>
2022-10-23 22:55:59 +0200

    🔴 BookResource should inject JpaBookDao (duuuhhh! this isn't right)

--- src/main/java/no/kristiania/library/LibraryResourceConfig.java
+++ src/main/java/no/kristiania/library/LibraryResourceConfig.java
@@ -1,5 +1,7 @@
 package no.kristiania.library;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
 import no.kristiania.library.database.BookDao;
 import no.kristiania.library.database.jpa.JpaBookDao;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
@@ -10,11 +12,13 @@ import javax.sql.DataSource;
 class LibraryResourceConfig extends ResourceConfig {
     public LibraryResourceConfig(DataSource dataSource) {
         super(BookResource.class);
+        var entityManagerFactory = Persistence.createEntityManagerFactory("library");
         register(new AbstractBinder() {
             @Override
             protected void configure() {
                 bind(JpaBookDao.class).to(BookDao.class);
-                bindFactory(() -> dataSource).to(DataSource.class);
+                bindFactory(entityManagerFactory::createEntityManager)
+                        .to(EntityManager.class);
             }
         });
     }
