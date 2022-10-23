d59ccc6f8c8ea99dfef6cc94d02d446c1743e011
Johannes Brodwall <johannes@brodwall.com>
2022-10-23 22:07:04 +0200

    🔴 Library server should serve front page (wrong content)

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -1,6 +1,9 @@
 package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,6 +13,11 @@ public class LibraryServer {
 
     public LibraryServer(int port) {
         this.server = new Server(0);
+
+        var handler = new WebAppContext();
+        handler.setContextPath("/");
+        handler.setBaseResource(Resource.newClassPathResource("/webapp"));
+        server.setHandler(handler);
     }
 
     public void start() throws Exception {
