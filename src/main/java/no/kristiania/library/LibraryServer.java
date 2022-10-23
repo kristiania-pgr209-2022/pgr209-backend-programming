58aefafbad00bfe6bb75b4a91f55a4d1ca110df5
Johannes Brodwall <johannes@brodwall.com>
2022-10-23 22:15:58 +0200

    🔴 LibraryServerTest should post new book (InjectionManagerFactory not found)

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -2,8 +2,11 @@ package no.kristiania.library;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,6 +20,7 @@ public class LibraryServer {
         var handler = new WebAppContext();
         handler.setContextPath("/");
         handler.setBaseResource(Resource.newClassPathResource("/webapp"));
+        handler.addServlet(new ServletHolder(new ServletContainer(new ResourceConfig(BookResource.class))), "/api/*");
         server.setHandler(handler);
     }
 
