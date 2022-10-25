879f597d3adff8b3cc670a876b64150c7b2237d4
Johannes Brodwall <johannes@brodwall.com>
2022-10-25 10:17:03 +0200

    Extract interface LibraryDao from JdbcLibraryDao

---
+++ src/main/java/no/kristiania/library/database/JdbcLibraryDao.java
@@ -0,0 +1,47 @@
+package no.kristiania.library.database;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcLibraryDao implements LibraryDao {
+    private final DataSource dataSource;
+
+    public JdbcLibraryDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void save(Library library) throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            var sql = "insert into libraries (name, address) values (?, ?)";
+            try (var statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                statement.setString(1, library.getName());
+                statement.setString(2, library.getAddress());
+                statement.executeUpdate();
+
+                try (var generatedKeys = statement.getGeneratedKeys()) {
+                    generatedKeys.next();
+                    library.setId(generatedKeys.getLong(1));
+                }
+            }
+        }
+    }
+
+    @Override
+    public Library retrieve(long id) throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            try (var statement = connection.prepareStatement("select * from libraries where id = ?")) {
+                statement.setLong(1, id);
+                try (var rs = statement.executeQuery()) {
+                    rs.next();
+                    var library = new Library();
+                    library.setId(rs.getLong("id"));
+                    library.setName(rs.getString("name"));
+                    library.setAddress(rs.getString("address"));
+                    return library;
+                }
+            }
+        }
+    }
+}
