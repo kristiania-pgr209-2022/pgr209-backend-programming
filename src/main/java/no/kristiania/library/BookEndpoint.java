2fda2919f1cd382e174620f86320c38a5a840de8
Johannes Brodwall <johannes@brodwall.com>
2022-10-25 15:43:40 +0200

    🟢 Library server should post and get books (in memory books)

--- src/main/java/no/kristiania/library/BookEndpoint.java
+++ src/main/java/no/kristiania/library/BookEndpoint.java
@@ -7,18 +7,22 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import no.kristiania.library.database.Book;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/books")
 public class BookEndpoint {
-    @POST
-    public void addBook() {
 
+    private static List<Book> allBooks = new ArrayList<>();
+
+    @POST
+    public void addBook(Book book) {
+        allBooks.add(book);
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Book> listAllBooks() {
-        return List.of();
+        return allBooks;
     }
 }
