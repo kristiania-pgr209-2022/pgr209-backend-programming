0ba940800176acfae786b9855594ac27dadfef30
Johannes Brodwall <johannes@brodwall.com>
2022-10-25 15:42:31 +0200

    🔴 Library server should post and get books (GET returns an empty)

--- src/main/java/no/kristiania/library/BookEndpoint.java
+++ src/main/java/no/kristiania/library/BookEndpoint.java
@@ -1,12 +1,24 @@
 package no.kristiania.library;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import no.kristiania.library.database.Book;
+
+import java.util.List;
 
 @Path("/books")
 public class BookEndpoint {
     @POST
     public void addBook() {
 
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Book> listAllBooks() {
+        return List.of();
     }
 }
