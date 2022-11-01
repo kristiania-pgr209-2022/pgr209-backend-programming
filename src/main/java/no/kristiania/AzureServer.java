a71bb251da9e864b1f8bd0d6235ceda22c91b081
Johannes Brodwall <johannes@brodwall.com>
2022-11-01 15:33:57 +0100

    🔴 Azure demo server should serve front page (No resourceBase or war set for context)

--- src/main/java/no/kristiania/AzureServer.java
+++ src/main/java/no/kristiania/AzureServer.java
@@ -1,6 +1,8 @@
 package no.kristiania;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,6 +12,14 @@ public class AzureServer {
 
     public AzureServer(int port) {
         server = new Server(port);
+        server.setHandler(createWebAppContext());
+    }
+
+    private WebAppContext createWebAppContext() {
+        var context = new WebAppContext();
+        context.setContextPath("/");
+        context.setBaseResource(Resource.newClassPathResource("/azure-demo-webapp"));
+        return context;
     }
 
     public void start() throws Exception {
