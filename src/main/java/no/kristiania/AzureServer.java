ccc883f23da87b9b393a2f6c04d5b0c04cf00342
Johannes Brodwall <johannes@brodwall.com>
2022-11-01 15:30:19 +0100

    🔴 Azure demo server should serve front page (fails to run)

---
+++ src/main/java/no/kristiania/AzureServer.java
@@ -0,0 +1,16 @@
+package no.kristiania;
+
+import java.net.URL;
+
+public class AzureServer {
+    public AzureServer(int port) {
+    }
+
+    public void start() {
+
+    }
+
+    public URL getURL() {
+        return null;
+    }
+}
--- src/test/java/no/kristiania/AzureServerTest.java
+++ src/test/java/no/kristiania/AzureServerTest.java
@@ -2,16 +2,21 @@ package no.kristiania;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AzureServerTest {
 
     @Test
-    void shouldServerFrontPage() {
+    void shouldServeFrontPage() throws IOException {
         var server = new AzureServer(8080);
         server.start();
 
         var connection = (HttpURLConnection)server.getURL().openConnection();
-        assertThat(connection.getResponseCode()).isEqualTo(200);
+        assertThat(connection.getResponseCode())
+                .as(connection.getResponseMessage())
+                .isEqualTo(200);
     }
 }
