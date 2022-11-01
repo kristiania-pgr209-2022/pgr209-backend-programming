53d0f9790b99409447e2bde1162111e1b942af81
Johannes Brodwall <johannes@brodwall.com>
2022-11-01 10:04:36 +0100

    Make jetty read file resources

--- src/main/java/no/kristiania/library/LibraryServer.java
+++ src/main/java/no/kristiania/library/LibraryServer.java
@@ -5,6 +5,7 @@ import jakarta.servlet.DispatcherType;
 import no.kristiania.library.database.Database;
 import org.eclipse.jetty.plus.jndi.Resource;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -15,6 +16,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.EnumSet;
@@ -24,14 +26,20 @@ public class LibraryServer {
 
     private final Server server;
 
-    public LibraryServer(int port, DataSource dataSource) throws NamingException {
+    public LibraryServer(int port, DataSource dataSource) throws NamingException, IOException {
         this.server = new Server(port);
 
         var handler = new WebAppContext();
         handler.setContextPath("/");
-        handler.setBaseResource(
-                org.eclipse.jetty.util.resource.Resource.newClassPathResource("/webapp")
-        );
+        var baseResource = org.eclipse.jetty.util.resource.Resource.newClassPathResource("/webapp");
+        org.eclipse.jetty.util.resource.Resource sourceResource = getSourceResource(baseResource);
+
+        if (sourceResource != null) {
+            handler.setBaseResource(sourceResource);
+            handler.setInitParameter(DefaultServlet.CONTEXT_INIT + "useFileMappedBuffer", "false");
+        } else {
+            handler.setBaseResource(baseResource);
+        }
 
         new Resource("jdbc/dataSource", dataSource);
         var entityManagerFactory = Persistence.createEntityManagerFactory("library");
@@ -40,6 +48,13 @@ public class LibraryServer {
         handler.addServlet(new ServletHolder(new ServletContainer(config)), "/api/*");
         handler.addFilter(new FilterHolder(new EntityManagerFilter(config)), "/api/*", EnumSet.of(DispatcherType.REQUEST));
         server.setHandler(handler);
+    }
+
+    private org.eclipse.jetty.util.resource.Resource getSourceResource(org.eclipse.jetty.util.resource.Resource baseResource) throws IOException {
+        return org.eclipse.jetty.util.resource.Resource.newResource(baseResource.getFile().toString()
+                .replace('\\', '/')
+                .replace("target/classes", "src/main/resources")
+        );
     }
 
     public void start() throws Exception {
