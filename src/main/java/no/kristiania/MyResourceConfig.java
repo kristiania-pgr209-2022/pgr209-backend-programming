c763b91117be6041009dbff72cc90b60bd517c6d
Johannes Brodwall <johannes@brodwall.com>
2022-11-04 00:31:39 +0100

    Move DataSource creation to separate class

--- src/main/java/no/kristiania/MyResourceConfig.java
+++ src/main/java/no/kristiania/MyResourceConfig.java
@@ -4,10 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.eclipse.jetty.plus.jndi.Resource;
-import org.flywaydb.core.Flyway;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.h2.jdbcx.JdbcDataSource;
 
 import javax.naming.NamingException;
 import java.util.Map;
@@ -21,11 +19,7 @@ class MyResourceConfig extends ResourceConfig {
     public MyResourceConfig() throws NamingException {
         super(BooksEndpoint.class);
         setProperties(Map.of("jersey.config.server.wadl.disableWadl", "true"));
-        var dataSource = new JdbcDataSource();
-        dataSource.setUrl("jdbc:h2:mem:test;MODE=LEGACY;DB_CLOSE_DELAY=-1");
-        var flyway = Flyway.configure().dataSource(dataSource).load();
-        flyway.migrate();
-        new Resource("jdbc/dataSource", dataSource);
+        new Resource("jdbc/dataSource", Database.createDataSource());
         entityManagerFactory = Persistence.createEntityManagerFactory("library");
         register(new AbstractBinder() {
             @Override
