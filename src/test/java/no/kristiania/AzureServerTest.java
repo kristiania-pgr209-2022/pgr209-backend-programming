db932ffca00cb3f00d192115e6d6a4d0f1b106eb
Johannes Brodwall <johannes@brodwall.com>
2022-11-03 23:29:10 +0100

    Refactor AzureServerTest for extension

--- src/test/java/no/kristiania/AzureServerTest.java
+++ src/test/java/no/kristiania/AzureServerTest.java
@@ -1,21 +1,33 @@
 package no.kristiania;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AzureServerTest {
 
+    private AzureServer server;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        server = new AzureServer(0);
+        server.start();
+    }
+
     @Test
     void shouldServeFrontPage() throws Exception {
-        var server = new AzureServer(0);
-        server.start();
-
-        var connection = (HttpURLConnection)server.getURL().openConnection();
+        var connection = openConnection();
         assertThat(connection.getResponseCode())
                 .as(connection.getResponseMessage())
                 .isEqualTo(200);
+    }
+
+    private HttpURLConnection openConnection() throws IOException {
+        return (HttpURLConnection) new URL(server.getURL(), "/").openConnection();
     }
 }
