e9f0741797506444baa41d8f7c68b85fd92f8727
Johannes Brodwall <johannes@brodwall.com>
2022-11-03 23:52:45 +0100

    🔴 Book dao should retrieve saved book (add EntityManager code)

--- src/main/java/no/kristiania/BookDao.java
+++ src/main/java/no/kristiania/BookDao.java
@@ -1,11 +1,20 @@
 package no.kristiania;
 
-public class BookDao {
-    public void save(Book book) {
+import jakarta.persistence.EntityManager;
 
+public class BookDao {
+
+    private final EntityManager entityManager;
+
+    public BookDao(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
-    public Book retrieve(Long id) {
-        return null;
+    public void save(Book book) {
+        entityManager.persist(book);
+    }
+
+    public Book retrieve(long id) {
+        return entityManager.find(Book.class, id);
     }
 }
--- src/test/java/no/kristiania/BookDaoTest.java
+++ src/test/java/no/kristiania/BookDaoTest.java
@@ -1,15 +1,27 @@
 package no.kristiania;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookDaoTest {
 
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        entityManager = Persistence
+                .createEntityManagerFactory("library")
+                .createEntityManager();
+    }
+
     @Test
     void shouldRetrieveSavedBook() {
         var book = sampleBook();
-        var dao = new BookDao();
+        var dao = new BookDao(entityManager);
         dao.save(book);
         flush();
         assertThat(dao.retrieve(book.getId()))
@@ -21,10 +33,13 @@ public class BookDaoTest {
     }
 
     private Book sampleBook() {
-        return null;
+        var book = new Book();
+        book.setAuthorName("Johannes Brodwall");
+        book.setTitle("The Code and the Coder");
+        return book;
     }
 
     private void flush() {
-
+        entityManager.flush();
     }
 }
