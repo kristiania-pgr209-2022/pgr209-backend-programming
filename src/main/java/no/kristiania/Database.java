874a7745ceede4ab0fa96da4c4246feb51f10cba
Johannes Brodwall <johannes@brodwall.com>
2022-11-04 00:39:13 +0100

    Use MS SQL database driver

--- src/main/java/no/kristiania/Database.java
+++ src/main/java/no/kristiania/Database.java
@@ -1,14 +1,16 @@
 package no.kristiania;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import org.flywaydb.core.Flyway;
-import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.DataSource;
 
 public class Database {
     public static DataSource createDataSource() {
-        var dataSource = new JdbcDataSource();
-        dataSource.setUrl("jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true");
+        var dataSource = new SQLServerDataSource();
+        dataSource.setURL("jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true");
+        dataSource.setUser("sa");
+        dataSource.setPassword("yourStrong(123)Password");
         var flyway = Flyway.configure().dataSource(dataSource).load();
         flyway.migrate();
         return dataSource;
