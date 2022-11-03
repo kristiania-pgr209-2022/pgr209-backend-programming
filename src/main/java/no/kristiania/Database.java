381544064a88f3a7845a498a45c18a7156305517
Johannes Brodwall <johannes@brodwall.com>
2022-11-04 00:51:26 +0100

    Use MS SQL database driver with HikariCP

--- src/main/java/no/kristiania/Database.java
+++ src/main/java/no/kristiania/Database.java
@@ -1,16 +1,16 @@
 package no.kristiania;
 
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 import org.flywaydb.core.Flyway;
 
 import javax.sql.DataSource;
 
 public class Database {
     public static DataSource createDataSource() {
-        var dataSource = new SQLServerDataSource();
-        dataSource.setURL("jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true");
-        dataSource.setUser("sa");
-        dataSource.setPassword("yourStrong(123)Password");
+        var dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("yourStrong123Password");
         var flyway = Flyway.configure().dataSource(dataSource).load();
         flyway.migrate();
         return dataSource;
