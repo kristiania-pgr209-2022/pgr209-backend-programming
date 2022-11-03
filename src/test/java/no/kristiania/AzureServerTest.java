969a54c214c64e8be029def18dd6e2dc7be1e487
Johannes Brodwall <johannes@brodwall.com>
2022-11-03 23:33:33 +0100

    🔴 Library server should return posted books

--- src/test/java/no/kristiania/AzureServerTest.java
+++ src/test/java/no/kristiania/AzureServerTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AzureServerTest {
@@ -21,13 +23,36 @@ public class AzureServerTest {
 
     @Test
     void shouldServeFrontPage() throws Exception {
-        var connection = openConnection();
+        var connection = openConnection("/");
         assertThat(connection.getResponseCode())
                 .as(connection.getResponseMessage())
                 .isEqualTo(200);
     }
 
-    private HttpURLConnection openConnection() throws IOException {
-        return (HttpURLConnection) new URL(server.getURL(), "/").openConnection();
+    @Test
+    void shouldGetPostedBook() throws IOException {
+        var postConnection = openConnection("/api/books");
+        postConnection.setRequestMethod("POST");
+        postConnection.setRequestProperty("Content-Type", "application/json");
+        postConnection.setDoOutput(true);
+        postConnection.getOutputStream().write("""
+            {
+                "authorName": "David Flanagan",
+                "title": "Java in a Nutshell"
+            }
+            """.getBytes(UTF_8));
+        assertThat(postConnection.getResponseCode()).isEqualTo(204);
+
+        var getConnection = openConnection("/api/books");
+        assertThat(getConnection.getResponseCode()).isEqualTo(200);
+        assertThat(getConnection.getInputStream()).asString(UTF_8)
+                .contains("David Flanagan")
+                .contains("Java in a Nutshell");
+    }
+
+
+
+    private HttpURLConnection openConnection(String path) throws IOException {
+        return (HttpURLConnection) new URL(server.getURL(), path).openConnection();
     }
 }
