5e9e3709582134645f1b787bc45e057abe67a760
Johannes Brodwall <johannes@brodwall.com>
2022-11-04 00:55:57 +0100

    Move datasource configuration to external file

--- src/main/java/no/kristiania/Database.java
+++ src/main/java/no/kristiania/Database.java
@@ -4,13 +4,20 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.flywaydb.core.Flyway;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Database {
-    public static DataSource createDataSource() {
+    public static DataSource createDataSource() throws IOException {
+        var props = new Properties();
+        try (var fileInputStream = new FileInputStream("application.properties")) {
+            props.load(fileInputStream);
+        }
         var dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl("jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("yourStrong123Password");
+        dataSource.setJdbcUrl(props.getProperty("dataSource.url"));
+        dataSource.setUsername(props.getProperty("dataSource.username"));
+        dataSource.setPassword(props.getProperty("dataSource.password"));
         var flyway = Flyway.configure().dataSource(dataSource).load();
         flyway.migrate();
         return dataSource;
