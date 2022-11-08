2588ea26b434c31e8d062fbf84797ba5a6fa5b4b
Johannes Brodwall <johannes@brodwall.com>
2022-11-08 09:37:40 +0100

    🔴 Library server should GET POST-ed books (Injection manager not found)

---
+++ src/main/java/no/kristiania/BookEndpoint.java
@@ -0,0 +1,13 @@
+package no.kristiania;
+
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+@Path("/books")
+public class BookEndpoint {
+
+    @POST
+    public void addBook() {
+
+    }
+}
