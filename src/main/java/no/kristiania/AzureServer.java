9f08979df6c4d2043d71b134c8035b7206ad17ec
Johannes Brodwall <johannes@brodwall.com>
2022-11-08 14:32:30 +0100

    Setup connection to Ms SQL in main method

--- src/main/java/no/kristiania/AzureServer.java
+++ src/main/java/no/kristiania/AzureServer.java
@@ -1,21 +1,27 @@
 package no.kristiania;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.servlet.DispatcherType;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.flywaydb.core.Flyway;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.EnumSet;
 import java.util.Optional;
+import java.util.Properties;
 
 public class AzureServer {
     private static final Logger logger = LoggerFactory.getLogger(AzureServer.class);
@@ -48,9 +54,25 @@ public class AzureServer {
         var port = Optional.ofNullable(System.getenv("HTTP_PLATFORM_PORT"))
                 .map(Integer::parseInt)
                 .orElse(8080);
-        var server = new AzureServer(port, null);
+        var server = new AzureServer(port, createDataSource());
         server.start();
         logger.info("Started at {}", server.getURL());
     }
 
+    private static DataSource createDataSource() throws IOException, SQLException {
+        var properties = new Properties();
+        try (var config = new FileInputStream("application.properties")) {
+            properties.load(config);
+        }
+
+        var dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(properties.getProperty("dataSource.url"));
+        dataSource.setUsername(properties.getProperty("dataSource.username"));
+        dataSource.setPassword(properties.getProperty("dataSource.password"));
+        dataSource.setLoginTimeout(2000);
+
+        Flyway.configure().dataSource(dataSource).load().migrate();
+
+        return dataSource;
+    }
 }
