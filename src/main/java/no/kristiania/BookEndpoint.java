0ab760de1d09d6e6bc5434d013ce1bdb68f8789a
Johannes Brodwall <johannes@brodwall.com>
2022-11-08 09:46:43 +0100

    🔴 Library server should GET POST-ed books (added @Consumes)

--- src/main/java/no/kristiania/BookEndpoint.java
+++ src/main/java/no/kristiania/BookEndpoint.java
@@ -1,8 +1,11 @@
 package no.kristiania;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +16,7 @@ public class BookEndpoint {
     private final List<Book> bookList = new ArrayList<>();
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     public void addBook(Book book) {
         bookList.add(book);
     }
