ef0d1e0d379bfb8f23f310b5295a3a45d0e14a7e
Johannes Brodwall <johannes@brodwall.com>
2022-11-08 10:53:48 +0100

    🔴 BookEndpoint should use DAO (Cannot acquire data source)

--- src/main/java/no/kristiania/MyResourceConfig.java
+++ src/main/java/no/kristiania/MyResourceConfig.java
@@ -1,5 +1,8 @@
 package no.kristiania;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -7,10 +10,14 @@ class MyResourceConfig extends ResourceConfig {
     public MyResourceConfig() {
         super(BookEndpoint.class);
 
+        var entityManagerFactory = Persistence.createEntityManagerFactory("library");
+
         register(new AbstractBinder() {
             @Override
             protected void configure() {
                 bind(BookDao.class).to(BookDao.class);
+                bindFactory(entityManagerFactory::createEntityManager)
+                        .to(EntityManager.class);
             }
         });
     }
