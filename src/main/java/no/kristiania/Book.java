eb61671014b6c1aba7180ed7b47cb34b6ee070ce
Johannes Brodwall <johannes@brodwall.com>
2022-11-08 10:33:56 +0100

    🔴 Book DAO should list saved books (Table "BOOK" not found (this database is empty))

--- src/main/java/no/kristiania/Book.java
+++ src/main/java/no/kristiania/Book.java
@@ -1,9 +1,14 @@
 package no.kristiania;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String authorName;
